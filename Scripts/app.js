f9e708b2100a75bc198ab94b535aab55f68d10db
dherrera-code <dherr929@gmail.com>
2026-02-05 14:38:20 -0800

    implemented a toggling sidebar to display favorite pokemon!

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,18 @@
 // alert("please work")
+
+const toggleFavBtn = document.getElementById("toggleFavBtn")
+const favoritesSidebar = document.getElementById("favoritesSidebar")
+const hideFavSidebarBtn = document.getElementById("hideFavSidebarBtn");
+
+
+
+
+
+
+toggleFavBtn.addEventListener("click", () => {
+    console.log("Fav btn clicked!")
+    favoritesSidebar.classList.remove("hidden")
+})
+hideFavSidebarBtn.addEventListener("click", () =>{
+    favoritesSidebar.classList.add("hidden")
+})
