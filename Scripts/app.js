109490ec8776df87fbd79b0f588810fb95233093
dherrera-code <dherr929@gmail.com>
2026-02-05 15:24:09 -0800

    Main content is implemented, starting to get data and implement responsiveness for tablet and mobile!

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,18 +1,38 @@
 // alert("please work")
 
-const toggleFavBtn = document.getElementById("toggleFavBtn")
+const toggleFavSidebarBtn = document.getElementById("toggleFavSidebarBtn")
 const favoritesSidebar = document.getElementById("favoritesSidebar")
 const hideFavSidebarBtn = document.getElementById("hideFavSidebarBtn");
 
+const pokemonCries = document.getElementById("pokemonCries")
+const pokemonCriesBtn = document.getElementById("pokemonCriesBtn")
+const favPokemonStar = document.getElementById("favPokemonStar")
 
+let pokemon = "eevee";
+let isFav = false;
+const getPokemon = async (pokemon) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/eevee`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+getPokemon()
 
-
-
-
-toggleFavBtn.addEventListener("click", () => {
+toggleFavSidebarBtn.addEventListener("click", () => {
     console.log("Fav btn clicked!")
     favoritesSidebar.classList.remove("hidden")
 })
 hideFavSidebarBtn.addEventListener("click", () =>{
     favoritesSidebar.classList.add("hidden")
+})
+pokemonCriesBtn.addEventListener("click", () => {
+    pokemonCries.play()
+})
+favPokemonStar.addEventListener("click", () => {
+    //If not in favorite list then add to fav list and toggle fav star!
+    //else remove from fav and toggle nonFav star!
+    isFav = !isFav;
+    if(isFav) favPokemonStar.src = "./Assets/Star-Yellow.png"
+    else favPokemonStar.src = "./Assets/Star 1.png"
+
 })
